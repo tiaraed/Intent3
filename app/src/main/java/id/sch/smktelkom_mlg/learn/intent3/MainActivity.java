c0644ea770fab854d936daeecd799b48260ee8e3
Mokleters <tiara.eka99@gmail.com>
2016-10-09 22:32:01 +0700

    Intent SMS

--- app/src/main/java/id/sch/smktelkom_mlg/learn/intent3/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/intent3/MainActivity.java
@@ -20,6 +20,21 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        findViewById(R.id.imageViewSMS).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                composeSmsMessage("Pesan Dari SMK Telkom Malang");
+            }
+        });
+
+    }
+
+    private void composeSmsMessage(String message) {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra("sms_body", message);
+        if (intent.resolveActivity(getPackageManager()) != null)
+            startActivity(intent);
     }
 
     private void dialPhoneNumber(String phoneNumber) {
